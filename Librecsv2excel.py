fcfdd877a65eb59c57fe6621b64a9f913741058a
SubduedCrane651 <rchrdperreault@gmail.com>
2025-10-29 06:38:18 -0400

    Added print Excel file path

--- Librecsv2excel.py
+++ Librecsv2excel.py
@@ -32,6 +32,7 @@ def process_csv():
     excel_file = save_paths["excel_file"]
     if not os.path.isabs(excel_file):
         excel_file=os.path.join(os.path.expanduser("~"), "Documents")+"\\"+excel_file
+    print("Excel file path:", excel_file)
 
     image_file = save_paths["image_file"]
     if not os.path.isabs(image_file):
